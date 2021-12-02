08eca5b60550ba83ce4eda0e313ed0f994e389c2
Ian Duvall <iduvall@umich.edu>
2021-12-02 11:07:40 -0600

    day 02

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -76,9 +76,9 @@ Both parts: 1.4300000000000002ms
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.94ms
+Time part 2: 1.34ms
+Both parts: 3.2800000000000002ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 1.4300000000000002ms
+Total stars: 4/50
+Total time: 4.71ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.ts
@@ -0,0 +1,75 @@
+import run from "aocrunner";
+
+const parseInput = (rawInput: string) => rawInput.split('\n').map(line => {
+  const [direction, distance] = line.split(' ');
+  return [direction, parseInt(distance, 10)] as ['forward' | 'down' | 'up', number];
+});
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let horizontal = 0;
+  let vertical = 0;
+
+  const directions = input.forEach(([direction, distance]) => {
+    if (direction === 'forward') {
+      horizontal += distance;
+    } else if (direction === 'down') {
+      vertical += distance;
+    } else if (direction === 'up') {
+      vertical -= distance;
+    }
+  });
+
+  return horizontal * vertical;
+};
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  let horizontal = 0;
+  let vertical = 0;
+  let aim = 0;
+
+  const directions = input.forEach(([direction, distance]) => {
+    if (direction === 'forward') {
+      horizontal += distance;
+      vertical += aim * distance;
+    } else if (direction === 'down') {
+      aim += distance;
+    } else if (direction === 'up') {
+      aim -= distance;
+    }
+    input.length < 100 && console.log({horizontal, vertical});
+  });
+
+  return horizontal * vertical;
+};
+
+run({
+  part1: {
+    tests: [
+      { input: `
+      forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2
+      `, expected: 150 },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      { input: `
+      forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2
+      `, expected: 900 },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+});
